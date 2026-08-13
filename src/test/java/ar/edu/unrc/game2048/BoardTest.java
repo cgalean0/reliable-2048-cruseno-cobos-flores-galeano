c5f908a9bb52451412a525136d67e90e4866b966
ValenCobos <cobosgonzalezvalentin@gmail.com>
2026-08-13 10:39:50 -0300

    Test para casos básicos de movimientos

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -65,4 +65,75 @@ public class BoardTest {
 
 		assertEquals(2, noEmptyCells);
 	}
+
+	@Test
+	public void testMoveUp(){
+		Board board = new Board();
+		clearBoard(board);
+
+		board.setCell(1, 0, new Cell(2));
+		board.setCell(3, 0, new Cell(2));
+
+		boolean moved = board.moveUp();
+
+		assertTrue(moved);
+		assertEquals(4, board.getCell(0, 0).getValue());
+
+
+	}
+
+	@Test
+	public void testMoveDown(){
+		Board board = new Board();
+		clearBoard(board);
+
+		board.setCell(1, 0, new Cell(2));
+		board.setCell(3, 0, new Cell(2));
+
+		boolean moved = board.moveDown();
+
+		assertTrue(moved);
+		assertEquals(4, board.getCell(3, 0).getValue());
+	}
+
+	@Test
+	public void testMoveRight(){
+		Board board = new Board();
+		clearBoard(board);
+
+		board.setCell(1, 0, new Cell(2));
+		board.setCell(1, 3, new Cell(2));
+
+		boolean moved = board.moveRight();
+
+		assertTrue(moved);
+		assertEquals(4, board.getCell(1, 3).getValue());
+	}
+
+	@Test
+	public void testMoveLeft(){
+		Board board = new Board();
+		clearBoard(board);
+
+		board.setCell(1, 0, new Cell(2));
+		board.setCell(1, 3, new Cell(2));
+
+		boolean moved = board.moveLeft();
+
+		assertTrue(moved);
+		assertEquals(4, board.getCell(1, 0).getValue());
+	}
+
+	// ---------------- Auxiliar Methods -------------------
+
+	/**
+	 * Set all tiles to empty
+	 */
+	private void clearBoard(Board board){
+		for(int i = 0; i < board.getSize(); i++){
+			for(int j = 0; j < board.getSize(); j++){
+				board.setCell(i, j, Cell.EMPTY);
+			}
+		}
+	}
 }
