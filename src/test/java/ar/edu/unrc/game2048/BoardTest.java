67e96430d00ed4faf1f3213e9265aaec97e90c81
cgalean0 <cngaleano56@gmail.com>
2026-08-13 13:40:05 -0300

    chore: identation fixed

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -93,10 +93,9 @@ public class BoardTest {
         assertEquals(expected, result);
     }
 
-	@Test 
+	@Test
 	public  void isLosingBoardFailedMergedInLastColumn(){
 		Board board = new Board();
-		
 		int[][] values = {
         {2, 4, 8, 8},
         {32, 64, 128, 256},
@@ -116,7 +115,6 @@ public class BoardTest {
 	@Test
 	public void moveDowndoesNotProccesTopRow(){
 		Board board = new Board();
-		
 		board.setCell(0, 0, new Cell(2));
     	board.setCell(1, 0, new Cell(2));
     	board.setCell(2, 0, Cell.EMPTY);
